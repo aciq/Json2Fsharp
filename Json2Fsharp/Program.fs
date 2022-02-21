da4edf3bf14ac5dbf4a7f1125e46d4437f5fdbd3
Ian Erik Varatalu <iavara@ttu.ee>
2022-02-22 01:22:32 +0200

    1.0.6

--- Json2Fsharp/Program.fs
+++ Json2Fsharp/Program.fs
@@ -1,52 +1,55 @@
-
-
 open System
 open System.IO
 open CommandLine
 open Json2Fsharp
 open Xamasoft.JsonClassGenerator
 
-[<Verb(name="gen",isDefault=true)>]
-type Options = {
-  [<Option('x', "noexamples", HelpText = "Dont write examples in xml tags")>] ``noexamples`` : bool;
-  [<Option('n', "namespace", Default="MyNamespace", HelpText = "Namespace")>] ``namespace`` : string;
-  [<Value(0, MetaName="input",Required=true, HelpText = "Input file path (e.g. file.json)")>] input : string;
-  [<Value(1, MetaName="output",Required=true, HelpText = "Output file path (e.g. types.fs)")>] output : string;
-}
+[<Verb(name = "gen", isDefault = true)>]
+type Options =
+    { [<Option('x', "noexamples", HelpText = "Dont write examples in xml tags")>]
+      noexamples: bool
+      [<Option('n', "namespace", Default = "MyNamespace", HelpText = "Namespace")>]
+      ``namespace``: string
+      [<Value(0, MetaName = "input", Required = true, HelpText = "Input file path (e.g. file.json)")>]
+      input: string
+      [<Value(1, MetaName = "output", Required = true, HelpText = "Output file path (e.g. types.fs)")>]
+      output: string }
 
 
 [<EntryPoint>]
 let main argv =
-    
-    let parser = new Parser(fun f -> f.HelpWriter <- Console.Out )
-    
-    let testargv =
-        [|
-            @"C:\Users\kast\source\myrepos\Json2Fs\Json2Fsharp\samples\sample2.json"
-            @"C:\Users\kast\source\myrepos\Json2Fs\Json2Fsharp\samples\out2.fs"
-        |]
-    let result = parser.ParseArguments<Options>(testargv)
-        
+
+    let parser =
+        new Parser(fun f -> f.HelpWriter <- Console.Out)
+
+    // debugging
+//    let testargv =
+//        [| __SOURCE_DIRECTORY__ + "/samples/sample2.json"
+//           __SOURCE_DIRECTORY__ + "/samples/out2.fsz" |]
+
+    let result = parser.ParseArguments<Options>(argv)
+
     match result with
     | :? CommandLine.Parsed<Options> as command ->
         let value = command.Value
-        let path       = value.input
+        let path = value.input
         let outputPath = value.output
-        let input      = File.ReadAllText(path);
+        let input = File.ReadAllText(path)
         let writer = FSharpCodeWriter()
         let generator = JsonClassGenerator()
         generator.ExamplesInDocumentation <- not value.noexamples
         generator.Namespace <- value.``namespace``
         generator.AttributeLibrary <- JsonLibrary.SystemTextJson
         generator.CodeWriter <- writer
-        
-        
+
+
         match generator.GenerateClasses(input) with
         | _, error when error <> "" -> failwith error
-        | generatedFile , _ -> File.WriteAllText(outputPath ,generatedFile.ToString())
-        
+        | generatedFile, _ -> File.WriteAllText(outputPath, generatedFile.ToString())
+
         printfn $"successfully created {value.output}"
-        
+
     | :? CommandLine.NotParsed<Options> as notparsed -> ()
     | _ -> failwithf $"unknown command: %A{result}"
+
     0
